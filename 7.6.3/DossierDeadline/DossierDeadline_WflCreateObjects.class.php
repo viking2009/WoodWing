b9b1c6b402889a1554bea530e9f133ad94842466
Mykola Vyshynskyi <viking2009@ua.fm>
2013-03-30 22:50:07 +0200

    comment logs

--- 7.6.3/DossierDeadline/DossierDeadline_WflCreateObjects.class.php
+++ 7.6.3/DossierDeadline/DossierDeadline_WflCreateObjects.class.php
@@ -15,32 +15,33 @@ class DossierDeadline_WflCreateObjects extends WflCreateObjects_EnterpriseConnec
 
     final public function runBefore( WflCreateObjectsRequest &$req )
     {
-        if (isset($req->Objects) && count($req->Objects) > 0) {
-            foreach ($req->Objects as $object) {
-            
-                if ($object->MetaData->BasicMetaData->Type == 'Dossier') {
-                    #now id yet
-                    //$id = $object->MetaData->BasicMetaData->ID;
+        if ( defined('IGNORE_DOSSIER_DEADLINE') && IGNORE_DOSSIER_DEADLINE == true )
+        {
+			if (isset($req->Objects) && count($req->Objects) > 0)
+			{
+				foreach ($req->Objects as $object)
+				{
+					if ($object->MetaData->BasicMetaData->Type == 'Dossier')
+					{
+						#$now = mktime();
+						#file_put_contents(OUTPUTDIRECTORY . $now .'-'.$id.'-before', print_r($object, true));           
 
-                    $now = mktime();
-                    file_put_contents(OUTPUTDIRECTORY . $now .'-'.$id.'-before', print_r($object, true));           
-
-                    require_once dirname(__FILE__) . '/DossierDeadlineUtils.class.php';
-                    $extraMetaData = DossierDeadlineUtils::getExtraMetaData($object->MetaData, 'DEADLINE');
-                    // DEADLINE is required field, so next expression always TRUE
-                    if (isset($extraMetaData->Values) && count($extraMetaData->Values) > 0) {
-                        $deadline = $extraMetaData->Values[0];
-                        // rewrite deadline
-                        #doesn't work, only changes response
-                        $object->MetaData->WorkflowMetaData->Deadline = $deadline;
-                        //DossierDeadlineUtils::setDeadline($id, $deadline);
-                    }
+						require_once dirname(__FILE__) . '/DossierDeadlineUtils.class.php';
+						$extraMetaData = DossierDeadlineUtils::getExtraMetaData($object->MetaData, 'DEADLINE');
+						// DEADLINE is required field, so next expression always TRUE
+						if (isset($extraMetaData->Values) && count($extraMetaData->Values) > 0)
+						{
+							$deadline = $extraMetaData->Values[0];
+							// rewrite deadline
+							$object->MetaData->WorkflowMetaData->Deadline = $deadline;
+						}
                     
-                    file_put_contents(OUTPUTDIRECTORY . $now .'-'.$id.'-after', print_r($object, true));
-                }
-            }   
-        }
-    }
+						#file_put_contents(OUTPUTDIRECTORY . $now .'-'.$id.'-after', print_r($object, true));
+					}
+				}   
+			}
+		}
+	}
 
     final public function runAfter( WflCreateObjectsRequest $req, WflCreateObjectsResponse &$resp ) {}
 
