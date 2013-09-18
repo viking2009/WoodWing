a120fdb315c1d6843b8edb553c2072c3dffc9d04
Mykola Vyshynskyi <viking2009@ua.fm>
2013-09-18 22:01:23 +0300

    Task #54 Parent metadata plugin
    - ParentMetadata_WflCreateObjects.class.php: RUNMODE_AFTER, fix extra
    metadata overrule

--- 8.2.0/config/plugins/ParentMetadata/ParentMetadata_WflCreateObjects.class.php
+++ 8.2.0/config/plugins/ParentMetadata/ParentMetadata_WflCreateObjects.class.php
@@ -16,28 +16,30 @@ require_once dirname(__FILE__) . '/ParentMetadataUtils.class.php';
 class ParentMetadata_WflCreateObjects extends WflCreateObjects_EnterpriseConnector {
 
 	final public function getPrio()      { return self::PRIO_DEFAULT; }
-    final public function getRunMode()   { return self::RUNMODE_BEFORE; }
+    final public function getRunMode()   { return self::RUNMODE_AFTER; }
 
-	final public function runBefore( WflCreateObjectsRequest &$req )
+	final public function runBefore( WflCreateObjectsRequest &$req ) {}
+
+	final public function runAfter( WflCreateObjectsRequest $req, WflCreateObjectsResponse &$resp )
 	{
-		if (isset($req->Objects) && count($req->Objects) > 0) {
+		if (isset($resp->Objects) && count($resp->Objects) > 0) {
 			$ticket = $req->Ticket;
 
-			foreach ($req->Objects as $object) {
-				$objectId = $object->MetaData->BasicMetaData->ID;
+			foreach ($resp->Objects as $object) {
 				$objectType = $object->MetaData->BasicMetaData->Type;
 
 				$supportedFormats = unserialize(PM_SUPPORTED_FORMATS);
 
 				if (in_array($objectType, $supportedFormats) && isset($object->Relations) && count($object->Relations) > 0) {							
 
-					foreach ($resp->Relations as $relation) {
+					foreach ($object->Relations as $relation) {
 						if ($relation->Type == 'Contained') {
 							$parentObjectType = ParentMetadataUtils::getObjectType($relation->Parent);
 				
 							if ($parentObjectType == 'Dossier') {
 								$dossierId = $relation->Parent;	
-		
+								$objectId = $relation->Child;
+
 								require_once BASEDIR.'/server/protocols/soap/WflClient.php';
 								$soapClient = new WW_SOAP_WflClient();
 
@@ -57,19 +59,8 @@ class ParentMetadata_WflCreateObjects extends WflCreateObjects_EnterpriseConnect
 								if (isset($objects) && count($objects) == 1) {
 									$dossier = $objects[0];
 							
-									$objectMetaData = $object->MetaData;
-									$extraMetaData = $dossier->MetaData;
-									if (isset($objectMetaData->ExtraMetaData)) {
-										$extraMetadataKeys = unserialize(PM_EXTRAMETADATA_KEYS);
-								
-										foreach($objectMetaData->ExtraMetaData as &$extraMetaData) {
-											if(in_array($extraMetaData->Property, $extraMetadataKeys)) {
-												$newExtraMetaData = ParentMetadataUtils::getExtraMetaData($extraMetadataForOverrule, $extraMetaData->Property);
-												$extraMetaData->Values = $newExtraMetaData->Values;
-											}
-										}
-            						}
-								}								
+									ParentMetadataUtils::overruleObjectProperties($ticket, $objectId, $dossier->MetaData);
+								}							
 							}
 						}
 					}
@@ -77,8 +68,6 @@ class ParentMetadata_WflCreateObjects extends WflCreateObjects_EnterpriseConnect
 			}
 		}
 	}
-
-	final public function runAfter( WflCreateObjectsRequest $req, WflCreateObjectsResponse &$resp ) {}
 
 	final public function runOverruled( WflCreateObjectsRequest $req ) {}
 	
