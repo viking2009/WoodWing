2d3ce0201036c700a9103bb465568bddf8ffcd37
Mykola Vyshynskyi <viking2009@ua.fm>
2013-09-20 14:51:39 +0300

    Bug #64 BUG My Plugins
    - myUpdateBeamerDispatcher.class.php: fix postProcess function for
    support external IDS server

--- 8.2.0/config/plugins/myUpdateBeamer/myUpdateBeamerDispatcher.class.php
+++ 8.2.0/config/plugins/myUpdateBeamer/myUpdateBeamerDispatcher.class.php
@@ -38,7 +38,7 @@ class myUpdateBeamerDispatcher{
             return;
         }
         $now=md5('mv'.$layoutId);
-        $workspaceWE=WEBEDITDIR."$now/";
+        $workspaceID=WEBEDITDIRIDSERV."$now/";
         if (!self::getLayoutDetails($layoutId,$layName,$layStorename,$layVersion)) {
             LogHandler::Log('myUpdateBeamer','ERROR','postProcess: cannot get info for layout. Id='.$layoutId);
             return;
@@ -53,7 +53,7 @@ class myUpdateBeamerDispatcher{
             $types=array();
             $page=$res['pagenumber'];
             LogHandler::Log('myUpdateBeamer','DEBUG','postProcess: page='.$page);
-            $JPEGsrc=$workspaceWE.$layoutId.(($i==1)?'':$i).'.jpg';
+            $JPEGsrc=$workspaceID.$layoutId.(($i==1)?'':$i).'.jpg';
             LogHandler::Log('myUpdateBeamer','DEBUG','postProcess: JPEGsrc='.$JPEGsrc);
             if (file_exists($JPEGsrc)) {
                 $dest=$layStorename.'-page'.$page;
@@ -79,7 +79,7 @@ class myUpdateBeamerDispatcher{
             } else {
                 LogHandler::Log('myUpdateBeamer','ERROR','postProcess: ERROR with InDesign Server, could not find image '.$JPEGsrc);
             }
-            $PDFsrc=$workspaceWE.$layoutId.$i.'.pdf';
+            $PDFsrc=$workspaceID.$layoutId.$i.'.pdf';
             LogHandler::Log('myUpdateBeamer','DEBUG','postProcess: PDFsrc='.$PDFsrc);
             if (file_exists($PDFsrc)) {
                 $dest=$layStorename.'-page'.$page;
@@ -95,7 +95,7 @@ class myUpdateBeamerDispatcher{
             $dbDriver->query($sql);
             $i++;
         }
-        $PDFsrc=$workspaceWE.$layoutId.'.pdf';
+        $PDFsrc=$workspaceID.$layoutId.'.pdf';
         if (copy($PDFsrc,$layStorename.'-output.'.$layVersion)) {
             $layTypes['output']='application/pdf';
             LogHandler::Log('myUpdateBeamer','DEBUG','postProcess: copied '.$PDFsrc.' to '.$layStorename.'-output.'.$layVersion);
